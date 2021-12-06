142d8f98d20838047fd72f296accc455b5d931b7
Tiago Ribeiro <48443405+TiagoR79@users.noreply.github.com>
2021-12-06 15:31:20 +0000

    added house listing

---
+++ houses.js
@@ -0,0 +1,14 @@
+export default [
+	{
+	  picture: '/images/1.jpeg',
+	  type: 'Entire house',
+	  town: 'New York',
+	  title: 'Beautiful flat in New York!'
+	},
+	{
+	  picture: '/images/2.jpeg',
+	  type: 'Entire house',
+	  town: 'Amsterdam',
+	  title: 'A flat in Amsterdam with a great view'
+	}
+  ]
